c6690c6a43a9ef99a94fd2d21e56db05adf3f065
Eze Gatica <ezequielgatica@gmail.com>
2021-04-22 10:48:55 -0300

    Empezando con la API mock

--- src/Mock/Mock.functions.js
+++ src/Mock/Mock.functions.js
@@ -1,3 +1,5 @@
+
+//exportamos la funcion root, esto se ejecutaria en "/" y se llama desde el router como Mock.root"
 export const root = async (req, res) => {
     res.json({
         message: "Bienvenido!",
--- src/Mock/Mock.routes.js
+++ src/Mock/Mock.routes.js
@@ -1,9 +1,11 @@
+//import de libs
 import { Router } from "express";
 const rutas = Router();
 
+//importar las funciones, se llaman con Mock.<Func>
 import * as Mock from './Mock.functions'
 
-
+//creamos la ruta, y le asignamos la funcion
 rutas.get('/', Mock.root) 
 
 export default rutas

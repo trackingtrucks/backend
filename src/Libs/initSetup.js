cdba170aed274a63e3756098da7fbd8ddac816a3
Eze Gatica <ezequielgatica@gmail.com>
2021-04-25 21:07:50 -0300

    Se crea un usuario con permisos de admin por defecto

--- src/Libs/initSetup.js
+++ src/Libs/initSetup.js
@@ -1,18 +1,43 @@
 import Role from '../Models/Role'
+import Usuario from '../Models/Usuario'
+import CONFIG from '../config'
 export const crearRoles = async () => {
     const count = await Role.estimatedDocumentCount()
-
-    if (count > 0) return;
-    try {
-        const values = await Promise.all([
-            new Role({ nombre: 'conductor' }).save(),
-            new Role({ nombre: 'admin' }).save(),
-            new Role({ nombre: 'gestor' }).save()
-        ])
-        console.log(values);
-    } catch (error) {
-        console.error(error)
+    const usuarioCount = await Usuario.estimatedDocumentCount()
+    if (count > 0) {
+    } else {
+        try {
+            const values = await Promise.all([
+                new Role({ nombre: 'conductor' }).save(),
+                new Role({ nombre: 'admin' }).save(),
+                new Role({ nombre: 'gestor' }).save()
+            ])
+            console.log(values);
+        } catch (error) {
+            console.error(error)
+        }
     }
+    if (usuarioCount > 0) {
+
+    } else {
+        try {
+            const adminRole = await Role.findOne({ nombre: "admin" })
+            const values = await Promise.all([
+                new Usuario({
+                    nombre: "Eze",
+                    apellido: "Gatica",
+                    email: "ezequielgatica@gmail.com",
+                    password: await Usuario.encriptarPassword(CONFIG.DEFAULT_ADMIN_PASSWORD),
+                    companyId: 'admins',
+                    roles: [adminRole._id]
+                }).save(),
+            ])
+            console.log(values);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
 
 
 }
--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ config();
 export default {
     DATABASE_URL: process.env.CONNECTION_URL || null,
     PORT: process.env.PORT || 5000,
-    SECRET: process.env.SECRET
+    SECRET: process.env.SECRET,
+    DEFAULT_ADMIN_PASSWORD: process.env.DEFAULT_ADMIN_PASSWORD
 }

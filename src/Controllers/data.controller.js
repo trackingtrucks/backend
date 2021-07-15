f0ef3bfbb34a08abd2c5d45a6e459fe7bbfedc1f
lucasucr <71725559+lucasucr@users.noreply.github.com>
2021-07-15 15:08:58 -0300

    Tratando de arreglar este error que hace que no se guarde nada

--- src/Controllers/data.controller.js
+++ src/Controllers/data.controller.js
@@ -4,12 +4,12 @@ import Vehiculo from '../Models/Vehiculo';
 export const subirDatosOBD = async (req, res) => {
     try {
         const { fuelLevel, RPM, speed, coolantTemperature, pendingTroubleCodes, kilometrosRecorridos } = req.body;
-        console.log(req.body);
+        if(!speed) return res.status(400).json({message: 'No se recibio la speed'});
         const _id = req.userData.vehiculoActual.id;
         const vehiculo = await Vehiculo.findById({ _id });
         if(!vehiculo) return res.status(400).json({ message: 'No está asignado a ningun vehiculo'})
         const nuevosDatosOBD = new DatosOBD2({ vehiculo: {id : vehiculo._id}, fuelLevel, RPM, speed, coolantTemperature, pendingTroubleCodes, kilometrosRecorridos });
-        const datosNuevos = await nuevosDatosOBD.save();
+        await nuevosDatosOBD.save();
         return res.status(200).json({ message: 'Datos subidos con exito' })
     } catch (error) {
         return res.status(500).json({ message: error.message })

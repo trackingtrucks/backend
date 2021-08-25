103b657a337e2ca0069a298ea5bf49b86d69747d
lucasucr <71725559+lucasucr@users.noreply.github.com>
2021-08-25 10:59:35 -0300

    Update user.routes.js

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -7,17 +7,16 @@ rutas.get('/', [auth.verifyTokenWithVehicleData, auth.onlyConductor], Users.getU
 rutas.post('/codigo/conductor', [verify.emailIsValid, verify.existeUsuarioOEmail, auth.verifyToken, auth.onlyGestor], Users.codigoConductor) //Crea un codigo de registro para conductor dentro de la compania
 rutas.post('/codigo/gestor', [verify.emailIsValid, verify.existeUsuarioOEmail, auth.verifyToken, auth.onlyGestor], Users.codigoGestor) //Crea un codigo de registro para gestor dentro de la compania
 
-rutas.post("/crearTurno", [auth.verifyToken, auth.onlyGestor, verify.turnoYaCreado, verify.fechaValida], Users.crearTurno) //Crea un turno
-
 rutas.post("/restablecer", [verify.emailIsValid], Users.restablecerContaseña); //Pedir un email de restablecimiento
 
 rutas.patch("/cambiar/contrasena/token", Users.cambiarContraseñaPorToken); //Cambia la contraseña con un token de uso unico enviado por email
 rutas.patch("/cambiar/contrasena/logueado", [auth.verifyTokenWithPassword], Users.cambiarContraseñaLogueado); //Cambia la contraseña estando logueado y pidiendo la contraseña vieja
 rutas.patch("/cambiar/perfil", [auth.verifyToken, verify.emailIsValid, verify.existeUsuarioOEmail], Users.editarUsuario); //Cambiar la info de usuario
 
-rutas.put('/asignarTurno', [auth.verifyToken, auth.onlyGestor, verify.conductorNoEncontrado, verify.turnoNoCreado], Users.asignarTurno);//Asignar un usuario a un turno
+rutas.post("/turno/crear", [auth.verifyToken, auth.onlyGestor, verify.turnoYaCreado, verify.fechaValida], Users.crearTurno) //Crea un turno
+rutas.put('/turno/asignar', [auth.verifyToken, auth.onlyGestor, verify.conductorNoEncontrado, verify.turnoNoCreado], Users.asignarTurno);//Asignar un usuario a un turno
 
-rutas.patch("/empezarEntrega", [auth.verifyToken, auth.onlyConductor], Users.empezarEntrega); //Empezar una entrega
-rutas.patch("/terminarEntrega", [auth.verifyToken, auth.onlyConductor], Users.terminarEntrega); //Finalizar una entrega
+rutas.patch("/entrega/empezar", [auth.verifyToken, auth.onlyConductor], Users.empezarEntrega); //Empezar una entrega
+rutas.patch("/entrega/terminar", [auth.verifyToken, auth.onlyConductor], Users.terminarEntrega); //Finalizar una entrega
 
 export default rutas

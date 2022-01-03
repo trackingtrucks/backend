ed82db7069ca25ad4b1b76c139da8370ff31277d
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2022-01-02 21:42:17 -0300

    cambiando el dominio a uno de prueba

--- src/email.js
+++ src/email.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(config.SENDGRID_API_KEY)
 async function enviarMail({ para, templateId, data }) {
   sgMail.send({
     to: para,
-    from: `Tracking Trucks 🚍 <trackingtrucksarg@gmail.com>`,
+    from: `Tracking Trucks 🚍 <noreply@em651.trackingtrucks.ezegatica.online>`,
     templateId: templateId,
     personalizations: [
       {
